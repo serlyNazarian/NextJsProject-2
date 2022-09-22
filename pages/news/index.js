b3342e9c19fb72b52bd7a822b2eaa29f269d9913
serlyNazarian <serlynazarian@gmail.com>
2022-09-22 18:37:01 +0400

    main

---
+++ pages/news/index.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import { Fragment } from "react";
+
+function NewsPage() {
+    return (
+        <Fragment>
+            <h1>The News Page</h1>
+            <ul>
+                <li><Link href={'/news/nextjs-is-a-great-framework'}>NextJs is a great framework</Link></li>
+                <li>something else</li>
+            </ul>
+        </Fragment>
+    )
+}
+
+export default NewsPage;
